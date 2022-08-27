69e507642d507ce39faca10122c9db5a55cbe979
khuynh16 <kelvinahuynh@gmail.com>
2022-08-26 23:06:57 -0400

    comment to add source of where initial logo design came from

--- LynnWoodPortfolio/lynn-wood-portfolio/src/Components/Logo/Logo.js
+++ LynnWoodPortfolio/lynn-wood-portfolio/src/Components/Logo/Logo.js
@@ -1,3 +1,6 @@
+// original logo leaf design utilized to create lotus flower logo came from @Seda on Figma!
+// source: https://www.figma.com/community/file/943843409604246251
+
 import styles from "./Logo.module.css";
 import { ReactComponent as Logo } from "../../Assets/Images/Logo.svg";
 import { useNavigate } from "react-router-dom";
