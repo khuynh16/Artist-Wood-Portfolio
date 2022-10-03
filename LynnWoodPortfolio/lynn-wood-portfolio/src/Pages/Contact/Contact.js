5459c35ee5d436ef5688d1540b4353b006eb8e5a
khuynh16 <kelvinahuynh@gmail.com>
2022-10-02 22:34:34 -0400

    added form submission, validation, and submission message in contact form

--- LynnWoodPortfolio/lynn-wood-portfolio/src/Pages/Contact/Contact.js
+++ LynnWoodPortfolio/lynn-wood-portfolio/src/Pages/Contact/Contact.js
@@ -3,8 +3,22 @@ import ReusableLogo from "../../Components/Logo/Logo";
 import styles from "./Contact.module.css";
 import contact_pic from "../../Assets/Images/Contact.jpg";
 import Button from "@mui/material/Button";
+import { useState } from "react";
 
 const Contact = () => {
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const [name, setName] = useState("");
+
+  const validSubmission = (event) => {
+    event.preventDefault();
+    console.log("Here!");
+    setFormSubmitted(!formSubmitted);
+  };
+
+  const updateName = (event) => {
+    setName(event.target.value);
+  };
+
   return (
     <>
       <Navbar />
@@ -17,32 +31,66 @@ const Contact = () => {
             src={contact_pic}
             alt={"working on beginning_ending project"}
           ></img>
-          <form className={styles.form}>
-            <label for="name">Name:</label>
-            <input type="text" id="name" name="firstname"></input>
-            <label for="email">Email Address:</label>
-            <input type="text" id="email" name="email"></input>
-            <label for="email">Subject:</label>
-            <input type="text" id="subject" name="subject"></input>
-            <label for="email">Message:</label>
-            <textarea
-              type="text"
-              id="message"
-              name="message"
-              rows="10"
-            ></textarea>
+          <form
+            className={styles.form}
+            style={{
+              display: formSubmitted ? "none" : "",
+            }}
+            // action="https://formsubmit.co/lynn.clover1206@gmail.com"
+            // method="POST"
+            onSubmit={validSubmission}
+          >
+            <div>
+              <label htmlFor="name">Name:</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                required
+                value={name}
+                onChange={updateName}
+              ></input>
+            </div>
+            <div>
+              <label htmlFor="email">Email Address:</label>
+              <input type="email" id="email" name="email" required></input>
+            </div>
+            <div>
+              <label htmlFor="email">Subject:</label>
+              <input type="text" id="subject" name="subject" required></input>
+            </div>
+            <div>
+              <label htmlFor="email">Message:</label>
+              <textarea
+                type="text"
+                id="message"
+                name="message"
+                rows="10"
+                required
+              ></textarea>
+            </div>
             <Button
               variant="contained"
               sx={{
                 fontFamily: "Lucida Sans Regular",
                 fontWeight: "bold",
-                fontSize: "1rem",
+                fontSize: "1.2rem",
                 mt: "1rem",
+                textTransform: "capitalize",
               }}
+              type="submit"
+              value="Submit"
             >
               Submit Message
             </Button>
           </form>
+          <div
+            className={styles.emailSubmissionMessage}
+            style={{ display: !formSubmitted ? "none" : "flex" }}
+          >
+            Thank you for your email, {name}! <br></br>
+            <br></br> I will get back to you soon!
+          </div>
         </div>
       </div>
     </>
